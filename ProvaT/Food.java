62cbad94622bc5175fd429a4175f0198080303c1
Bernardo Souza <83983141+alvimdev@users.noreply.github.com>
2022-11-14 14:50:14 -0300

    Delete ProvaT directory

--- ProvaT/Food.java
+++
@@ -1,7 +0,0 @@
-package ProvaT;
-
-public class Food extends Command{
-    public void orderFood(String order, double value){
-        super.Ordering(order, value);
-    }
-}
--- ProvaT/bwyty.java
+++
@@ -1,9 +0,0 @@
-package ProvaT;
-
-/* TRABALHO DE AEDS */
-// Restaurante: bwyty (Galês)
-// Made by Alvim (2021951426) and ...
-
-public class bwyty {
-    
-}
